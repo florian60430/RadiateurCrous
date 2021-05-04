64aca5a223316d040827de9a7d8a7c41971a3421
florian60430 <florian.wantelez@hotmail.com>
2021-05-04 09:01:16 +0200

    rangement

---
+++ Conception/Site/chauffage.php
@@ -0,0 +1,35 @@
+<?php 
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=crous;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+$reponse=$bdd->prepare('SELECT * FROM chauffages where ID_Appartement = :ID ORDER BY id DESC');
+$reponse->execute(array(
+    'ID'=> $_POST["ID"]
+));
+?>
+
+<html>
+
+
+    <head> </head>
+
+    <body>
+        <?php 
+            
+            while ($donnees2 = $reponse->fetch())
+            {
+                echo "<B>".htmlspecialchars ($donnees2['ID_Appartement'])."</B>"."<br>"; 
+                echo htmlspecialchars ($donnees2['Etat_Chauffage'])."<br>";   
+                
+            }   
+        ?>
+
+    </body>
+
+</html>
