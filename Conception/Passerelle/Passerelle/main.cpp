707508201285b5d259b405b315a64ced85cac57b
florian60430 <florian.wantelez@hotmail.com>
2021-05-06 09:36:51 +0200

    ppt

--- Conception/Passerelle/Passerelle/main.cpp
+++ Conception/Passerelle/Passerelle/main.cpp
@@ -6,25 +6,72 @@
 #include "passerelle.h"
 #include "radiateur.h"
 
+using namespace std;
+
 
 int main() {
-
-	int idRad, temp, idBat;
-	//Consigne à entrée
-	printf("Choisir id du radiateur: \n");
-	scanf_s("%d", &idRad);
-	printf("Choisir id du batiment: \n");
-	scanf_s("%d", &idBat);
-
-	printf("Quelle température voulez vous atteindre? \n");
-	scanf_s("%d", &temp);
-
-
-	if (idRad != 0 && temp != 0 && idBat != 0) {
-		radiateur* ConsigneRad;
-		ConsigneRad = new radiateur;
-		ConsigneRad->getConsigne(idRad, temp, idBat); //Recup les consigne entrée
 	
-	}
-	system("PAUSE");
+	int idRad, temp, idBat,menu;
+	bool Etat = 0;
+
+	do
+	{
+		cout << endl << " ----------------------- MENU -----------------------" << endl << endl;
+		cout << "1. Entrer consigne pour radiateur" << endl;
+		cout << "2. Affiche information radiateur" << endl;
+		cout << "0. Quitter" << endl << endl;
+		cout << "Choix : ";
+		cin >> menu;
+		cout << endl;
+
+		switch (menu)
+		{
+		case 0: break;
+
+		
+
+		case 1: cout << endl << "Choisir id du radiateur: \n";
+			cin >> idRad;
+			cout << endl;
+			cout << endl << "Choisir id du batiment: \n";
+			cin >> idBat;
+			cout << endl;
+			cout << endl << "Quelle température voulez vous atteindre? \n";
+			cin >> temp;
+			cout << endl;
+			if (idRad != 0 && temp != 0 && idBat != 0) {
+				radiateur* ConsigneRad;
+				ConsigneRad = new radiateur;
+				ConsigneRad->getConsigne(idRad, temp, idBat); //Recup les consigne entrée
+
+			}
+			break;
+
+		case 2:
+			cout << endl << "Choisir id du radiateur: \n";
+			cin >> idRad;
+			cout << endl << "Choisir id du batiment: \n";
+			cin >> idBat;
+			cout << endl;
+			if (idRad != 0 && idBat != 0) {
+				radiateur* InfoRad;
+				InfoRad = new radiateur;
+				InfoRad->getInfoRad(idRad, temp, idBat, Etat); //Affiche donner Radiateur
+
+			}
+
+			
+			break;
+
+		
+
+			
+		}
+
+	} while (menu != 0);
+
+	
+	//Consigne à entrée
+
+	
 }
--- Conception/Passerelle/Passerelle/radiateur.h
+++ Conception/Passerelle/Passerelle/radiateur.h
@@ -1,18 +1,18 @@
 #pragma once
 class radiateur
 {
-	private:
-		int idRadiateur;
-		int Temperature;
-		bool Etat;
-		int idBat;
+private:
+	int idRadiateur;
+	int Temperature;
+	bool Etat;
+	int idBat;
 
-	public:
+public:
 
-		radiateur();
-		~radiateur();
-		int getConsigne(int idRadiateur, int Temperature, int idBat); //Recupére consigne
-		int AllumeRad();
-		int sendXbee(int idRadiateur, int Temperature, bool Etat, int idBat); //Send les consigne au xbee
+	radiateur();
+	~radiateur();
+	int getConsigne(int idRadiateur, int Temperature, int idBat); //Recupére consigne
+	int getInfoRad(int idRadiateur, int Temperature, int idBat, bool Etat);
+	void AfficheInfoRad(int idRadiateur,int Temperature, int idBat, bool Etat);
 };
 
