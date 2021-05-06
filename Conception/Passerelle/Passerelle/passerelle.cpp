645881dea24193da63fb153d5f81ec3907835019
FGarcia98 <fgarcia@la-providence.net>
2021-05-06 08:21:38 +0200

    Rapport + classe xbee

--- Conception/Passerelle/Passerelle/passerelle.cpp
+++ Conception/Passerelle/Passerelle/passerelle.cpp
@@ -14,11 +14,24 @@ passerelle::~passerelle()
 
 void passerelle::setConsigne(int idRad, int temp, int idBat)
 {
-	printf("Success! Parametrage temperature envoyer a radiateur Id_radiateur: %d | Temperature : %d | Id_Batiment : %d \n",idRad, temp, idBat);
-	xbee* sendXbee;
-	sendXbee = new xbee;
-	sendXbee->send();
+	if (idRad != 0 && temp != 0 && idBat != 0)
+	{
+		printf(" Parametrage radiateur, Success!\n Id_radiateur: %d \n Temperature : %d \n Id_Batiment : %d \n\n", idRad, temp, idBat);
+		xbee* sendXbee;
+		sendXbee = new xbee;
+		int RetourSendXbee = sendXbee->send();
+		if (RetourSendXbee == 0) {
+			printf("Les consignes du radiateur envoyer avec success!\n");
+		}
+		else {
+			printf("Error ! consigne non envoyer!\n");
+		}
 
+	}
+	else {
+		printf("ERROR");
+	}
+	
 	
 
 }
--- Conception/Passerelle/Passerelle/xbee.h
+++ Conception/Passerelle/Passerelle/xbee.h
@@ -1,17 +1,21 @@
 #pragma once
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+
+
 class xbee
 {
 private:
-
+	int xbee_id;
 
 public:
 
 	xbee();
 	~xbee();
-	int send(); //Send les consigne au xbee
+	int ReceptionXbee();
+	int sendData(); //Send les consigne au xbee
+	int readData();
 };
-
