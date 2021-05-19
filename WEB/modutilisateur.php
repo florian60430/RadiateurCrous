c52b2ff1591e9f4b3d433773e75384eeea2e9fdf
amalter02 <56536314+amalter02@users.noreply.github.com>
2021-05-19 08:43:03 +0200

    Add files via upload

--- WEB/modutilisateur.php
+++ WEB/modutilisateur.php
@@ -18,16 +18,7 @@ $reponse= $bdd->query('SELECT * FROM admin ORDER BY ID ');
         <title>Document</title>
     </head>
     <body>
-        <?php
-            while ($donnees = $reponse->fetch())
-            {
-                echo htmlspecialchars ($donnees['ID'])."<br>"; 
-                echo htmlspecialchars ($donnees['user'])."<br>";   
-                echo htmlspecialchars ($donnees['mdp'])."<br>"; 
-                echo htmlspecialchars ($donnees['droit'])."<br>"; 
-                echo htmlspecialchars ($donnees['adresse_mail'])."<br>";
-                ?> 
-                <form action="" method="post">
+        <form action="" method="post">
                 <input type="text" name="ID" placeholder="ID">
                 <input type="text" name="user" placeholder="identifiant">
                 <input type="text" name="mdp" placeholder="mot de passe">
@@ -45,11 +36,18 @@ $reponse= $bdd->query('SELECT * FROM admin ORDER BY ID ');
                 $result =  $bdd->query("UPDATE admin SET user='".$_POST['user']."',mdp='".$_POST['mdp']."',droit='".$_POST['droit']."',adresse_mail='".$_POST['mail']."' where ID ='".$_POST['ID']."' ");
                 echo htmlspecialchars ($_POST['droitForm'])."<br>";
               
-                } 
-                
-                ?>
-            <?php
-            }      
-        ?>     
+                }    
+        
+                while ($donnees = $reponse->fetch())
+                {
+                    echo htmlspecialchars ($donnees['ID'])."<br>"; 
+                    echo htmlspecialchars ($donnees['user'])."<br>";   
+                    echo htmlspecialchars ($donnees['mdp'])."<br>"; 
+                    echo htmlspecialchars ($donnees['droit'])."<br>"; 
+                    echo htmlspecialchars ($donnees['adresse_mail'])."<br>";    
+                    ?>
+                <?php
+                }         
+        ?>              
     </body>
 </html>
