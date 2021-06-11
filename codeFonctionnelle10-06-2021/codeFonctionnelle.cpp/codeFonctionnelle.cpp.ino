faded0574f6e7db81cb77c67b3c2fc91feca2f3b
florian60430 <florian.wantelez@hotmail.com>
2021-06-11 10:05:03 +0200

    code arduino

--- codeFonctionnelle10-06-2021/codeFonctionnelle.cpp/codeFonctionnelle.cpp.ino
+++ codeFonctionnelle10-06-2021/codeFonctionnelle.cpp/codeFonctionnelle.cpp.ino
@@ -26,59 +26,27 @@ void setup() {
 
 void loop()
 {  
-      // read the incoming byte:
-     
      messageRecu = Serial.read();
-
-             
+         
       if (messageRecu != -1) 
      {
         if (messageRecu == 122) 
         {
           idDigit1 = Serial.read();
-          //Serial.print("id digit 1 :");
-          //Serial.println(idDigit1);
-
           idDigit2 = Serial.read();
-          //Serial.print("id digit 2 :");
-          //Serial.println(idDigit2);
-
           idDigit3 = Serial.read();
-          //Serial.print("id digit 3 : ");
-          //Serial.println(idDigit3);
 
          if (id == ((idDigit1-48) *100 + (idDigit2 -48) *10 + (idDigit3-48)))
          {
           isGood = true;
 
-           plus = Serial.read();
-         // Serial.print("plus : ");
-          //Serial.println(plus);
-          
-          digit1 = Serial.read();
-          //Serial.print("digit1 : ");
-          //Serial.println(digit1);
-       
+          plus = Serial.read();       
+          digit1 = Serial.read();       
           digit2 = Serial.read();
-          //Serial.print("digit2 : ");
-         // Serial.println(digit2);
-
           virgule = Serial.read();
-          //Serial.print("virgule : ");
-          //Serial.println(virgule);
-
           decimal1 = Serial.read();
-         // Serial.print("decimal1 : ");
-          //Serial.println(decimal1);
-
           decimal2 = Serial.read();
-          //Serial.print("decimal2 : ");
-          //Serial.println(decimal2);
-
           finDeChaine = Serial.read();
-          //Serial.print("fin de chaine : ");
-          //Serial.println(finDeChaine);
-
          }
          else
          {
@@ -92,54 +60,33 @@ void loop()
          
        if (id == isGood)
        {
-          
           if (finDeChaine == 33)
-          {      
-            //Serial.print("consigne reçu : ");
-                  
+          {       
             consigne = (digit1 - 48) * 10 + (digit2 - 48) + (decimal1-48) / 10 + (decimal2-48) / 100;
-            //Serial.println(consigne);
-          
           }
           else 
           {
-            //Serial.println("La chaine à été corompu");
           }
-
         }
         else 
         {
-          //Serial.print("l'id n'est pas le notre consigne actuel : ");
-          //Serial.println(consigne);
         }
-          
         }
         else 
         {
-          //Serial.println("Le caractère de début de chaine est incorrect");
         }
-        
      }
      else
      {
-       //Serial.print("Aucun message reçu, consigne actuel : ");
-       //Serial.println(consigne);
      }
-
-    // read the incoming byte:
-   // communication->reception_consigne();
-
-   
- //Serial.print("temperature : ");
  
  temp = donnee->Convert_temperature(analogRead(0));
- //Serial.println(temp);
+
   donnee->capteur::set_timer(80);
   
   int tempActuel = donnee->Convert_temperature(analogRead(0));
 
   int hors_gel = donnee->capteur::detecte_presence();
-
 
 
 char tmp[50];
@@ -155,7 +102,6 @@ if (result == 0)
 
   power = chauffe->consigne::consigne_de_chauffe(hors_gel, tempActuel, consigne, heure, power);
 
-  //Serial.println(power);
   compteur ++;
   delay(500); 
 } 
